e9197be9eed63c1c940d3c5635bfc521267be8aa
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2025-02-17 23:21:10 +0530

    done

---
+++ challenges/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # path("january",views.index),
+     path("<int:month>",views.monthly_challenges_by_num),
+    #  path("<int:month>",views.monthly_challenges_by_num),
+    path("<str:month>",views.monthly_challenge)
+    ]
---
+++ challenges/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
+
+# Create your views here.
+
+monthly_challenges={
+    "january":"Eat no meat for the entire month",
+    "february":"Walk for at least 20 minutes every day!",
+    "march":"Learn Django for at least 20 minutes every day!",
+    "april":"Eat no meat for the entire month",
+    "may":"Walk for at least 20 minutes every day!",
+    "june":"Learn Django for at least 20 minutes every day!",
+    "july":"Eat no meat for the entire month",
+    "august":"Walk for at least 20 minutes every day!",
+    "september":"Learn Django for at least 20 minutes every day!",
+    "october":"Eat no meat for the entire month",
+    "november":"Walk for at least 20 minutes every day!",
+    "december":"Learn to sing for at least 20 minutes every day!"
+}
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the challenges index.")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    try:
+        challenge_text = monthly_challenges[month]
+    except: 
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
+
+def monthly_challenges_by_num(request, month):
+    months=list(monthly_challenges.keys())
+    if month>len(months):
+        return HttpResponseNotFound("Invalid month")
+    redirect_month=months[month-1]
+    return HttpResponseRedirect(redirect_month)
